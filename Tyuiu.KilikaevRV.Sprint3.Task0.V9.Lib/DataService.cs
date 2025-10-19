472a8966c7c0e1d843ecb16df8f212ac3366c9d3
Kilikaevrv <coffeine5555@gmail.com>
2025-10-19 16:45:56 +0300

    Task0

--- Tyuiu.KilikaevRV.Sprint3.Task0.V9.Lib/DataService.cs
+++ Tyuiu.KilikaevRV.Sprint3.Task0.V9.Lib/DataService.cs
@@ -4,29 +4,24 @@ using tyuiu.cources.programming.interfaces.Sprint3;
 
 
 public class DataService : ISprint3Task0V9
-
 {
    
-        public double GetSumSeries(double x)
+        public double GetSumSeries(double value, int startValue, int stopValue)
+    {
+        double sum = 0;
+
+        for (int i = startValue; i <= stopValue; i++)
         {
-            double sum = 0;
-
-            for (int i = 1; i <= 15; i++)
-            {
-                double denominator = 3 + Math.Pow(x, i);
-                double term = Math.Pow(1 / denominator, i);
-                sum += term;
-            }
-
-            return Math.Round(sum, 2); // Округляем до 2 знаков после запятой
+            double denominator = 3 + Math.Pow(value, i);
+            double term = Math.Pow(1 / denominator, i);
+            sum += term;
         }
+
+        return Math.Round(sum, 2);
+    }
+}
     
 
 
-public double GetSumSeries(double value, int startValue, int stopValue)
-    {
-        throw new NotImplementedException();
-    }
-}
 
     
--- Tyuiu.KilikaevRV.Sprint3.Task0.V9.Test/DataServiceTest.cs
+++ Tyuiu.KilikaevRV.Sprint3.Task0.V9.Test/DataServiceTest.cs
@@ -1,18 +1,19 @@
 ﻿namespace Tyuiu.KilikaevRV.Sprint3.Task0.V9.Test;
-using Tyuiu.KilikaevRV.Sprint3.Task0.V9.Lib;
-
+using global::Tyuiu.KilikaevRV.Sprint3.Task0.V9.Lib;
 
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void GetSumSeries(double x)
+        public void ValidGetSumSeries()
         {
             DataService ds = new DataService();
-            
-            double res = 0.43;
-            
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 15;
+            double wait = 0.43;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            Assert.AreEqual(wait, res);
         }
     }
-
-
+}

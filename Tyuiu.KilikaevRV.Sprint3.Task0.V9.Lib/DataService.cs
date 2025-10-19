7977ab5331f57ecf8d2b40823cf2686417231a8e
Kilikaevrv <coffeine5555@gmail.com>
2025-10-19 16:32:04 +0300

    Task0

--- Tyuiu.KilikaevRV.Sprint3.Task0.V9.Lib/DataService.cs
+++ Tyuiu.KilikaevRV.Sprint3.Task0.V9.Lib/DataService.cs
@@ -4,22 +4,26 @@ using tyuiu.cources.programming.interfaces.Sprint3;
 
 
 public class DataService : ISprint3Task0V9
+
 {
-    public double GetSumSeries(double x)
-    {
-        double sum = 0;
-
-        // Цикл for от i=1 до i=15
-        for (int i = 1; i <= 15; i++)
+   
+        public double GetSumSeries(double x)
         {
-            double term = Math.Pow(1.0 / (3 + Math.Pow(x, i)), i);
-            sum += term;
+            double sum = 0;
+
+            for (int i = 1; i <= 15; i++)
+            {
+                double denominator = 3 + Math.Pow(x, i);
+                double term = Math.Pow(1 / denominator, i);
+                sum += term;
+            }
+
+            return Math.Round(sum, 2); // Округляем до 2 знаков после запятой
         }
+    
 
-        return Math.Round(sum, 3); // Округляем до 3 знаков после запятой
-    }
 
-    public double GetSumSeries(double value, int startValue, int stopValue)
+public double GetSumSeries(double value, int startValue, int stopValue)
     {
         throw new NotImplementedException();
     }
--- Tyuiu.KilikaevRV.Sprint3.Task0.V9.Test/DataServiceTest.cs
+++ Tyuiu.KilikaevRV.Sprint3.Task0.V9.Test/DataServiceTest.cs
@@ -2,20 +2,18 @@
 using Tyuiu.KilikaevRV.Sprint3.Task0.V9.Lib;
 
 
-
-    
-    
-        [TestClass]
-        public class DataServiceTest
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
         {
-            [TestMethod]
-            public void ValidGetSumSeries()
-            {
-                DataService ds = new DataService();
-                double x = 0.5;
-                double wait = 0.031; // Ожидаемое значение (можно уточнить расчетом)
-                double res = ds.GetSumSeries(x);
-                Assert.AreEqual(wait, res);
-            }
+            DataService ds = new DataService();
+            double x = 0.5;
+            double wait = 0.43;
+            double res = ds.GetSumSeries(x);
+            Assert.AreEqual(wait, res);
         }
-    
+    }
+
+
